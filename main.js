6e2c0edb963eefba15b46f4aca1390e3f961db24
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-15 19:29:53 -0500

    Adding Computer choice function to choose random value

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+
+function getComputerChoice(){
+    value = Math.floor((Math.random()*3)+1);
+    console.log(value);
+}
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+
+
