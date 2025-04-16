8cef226cc3ef54674cac16317a0c1b89c3aaba15
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-15 19:56:10 -0500

    Fixing type errors, adding 5 round gameplay

--- main.js
+++ main.js
@@ -1,38 +1,50 @@
-console.log("Hello World");
+
 
 let computerScore = 0;
 let humanScore = 0;
 function getComputerChoice(){
-    value = Math.floor((Math.random()*3)+1);
+    value = Math.floor((Math.random()*3));
     
     const choices = ["rock", "paper", "scissors"];
+    guess = choices[value];
+    console.log("Computer choice: " + guess);
     return choices[value];
+    
 }
 
 function getHumanChoice(){
     let guess = prompt("Enter your choice: ");
+    console.log("Human choice: " + guess);
     return guess;
 }
 
 function playRound(humanChoice, computerChoice){
-    let humanChoice = humanChoice.toLowerCase();
-    if ((computerChoice = "rock") && (humanChoice = "scissors")){
+    humanChoice = humanChoice.toLowerCase();
+    if ((computerChoice === "rock") && (humanChoice === "scissors")){
         console.log("Computer Wins!");
-    }else if((computerChoice = "paper")&&(humanChoice = "rock")){
+    }else if((computerChoice === "paper")&&(humanChoice === "rock")){
         console.log("Computer Wins!");
-    }else if((computerChoice = "scissors") &&(humanChoice = "paper")){
+    }else if((computerChoice === "scissors") &&(humanChoice === "paper")){
         console.log("Computer Wins!");
-    }else if((humanChoice="paper") &&(computerChoice = "rock")){
+    }else if((humanChoice==="paper") &&(computerChoice === "rock")){
         console.log("Human Wins!");
-    }else if((humanChoice="rock")&&(computerChoice="scissors")){
+    }else if((humanChoice==="rock")&&(computerChoice==="scissors")){
         console.log("Human Wins!");
-    }else if((humanChoice="scissors")&&(computerChoice="paper")){
+    }else if((humanChoice==="scissors")&&(computerChoice==="paper")){
         console.log("Human Wins!");
     }else {
         console.log("Draw!");
     }
 }
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+
+function playGame(){
+   
+    for(let i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+}
+playGame();
 
 
