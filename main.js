f450096c5eec37693a86deef71a82792687340e1
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-15 19:31:48 -0500

    Adding logic to take human choice

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 console.log("Hello World");
 
-
+computerScore = 0;
+humanScore = 0;
 function getComputerChoice(){
     value = Math.floor((Math.random()*3)+1);
     console.log(value);
+}
+
+function getHumanChoice(){
+    guess = prompt("Enter your choice");
+    console.log(guess);
 }
 getComputerChoice();
 getComputerChoice();
 getComputerChoice();
 getComputerChoice();
 getComputerChoice();
-
+getHumanChoice();
 
