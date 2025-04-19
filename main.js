36716a56ceb5fb04d6727627873f3c299403fb8c
AaBeimbornCyber <205240632+ABeimbornCyber@users.noreply.github.com>
2025-04-19 11:21:48 -0500

    Added Scoreboard and winner display after 5 rounds

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ let computerScore = 0;
 let humanScore = 0;
 let winner = '';
 let score = document.querySelector(".score");
+const h_scores = document.querySelector("#human_score");
+const c_scores = document.querySelector("#computer_score");
+const winner_disp = document.querySelector(".winner");
+let winnerTag = document.createElement("h1");
 
 function getComputerChoice(){
     value = Math.floor((Math.random()*3));
@@ -21,16 +25,22 @@ function playRound(humanChoice, computerChoice){
     humanChoice = humanChoice.toLowerCase();
     if ((computerChoice === "rock") && (humanChoice === "scissors")){
         winner = "Computer Wins!";
+        computerScore++;
     }else if((computerChoice === "paper")&&(humanChoice === "rock")){
         winner = "Computer Wins!";
+        computerScore++;
     }else if((computerChoice === "scissors") &&(humanChoice === "paper")){
         winner = "Computer Wins!";
+        computerScore++;
     }else if((humanChoice==="paper") &&(computerChoice === "rock")){
         winner = "Human Wins!";
+        humanScore++;
     }else if((humanChoice==="rock")&&(computerChoice==="scissors")){
         winner = "Human Wins!";
+        humanScore++;
     }else if((humanChoice==="scissors")&&(computerChoice==="paper")){
         winner = "Human Wins!";
+        humanScore++;
     }else {
         winner = "Draw!";
     }
@@ -43,6 +53,22 @@ function playRound(humanChoice, computerChoice){
     resultSection.appendChild(computerResult);
     resultSection.appendChild(humanResult);
     resultSection.appendChild(result);
+    showScore(humanScore, computerScore);
+}
+
+function showScore(h_score, c_score){
+    
+    h_scores.textContent = "Human Score: " + h_score;
+    c_scores.textContent = "Computer Score: " + c_score;
+    if(h_score === 5){
+        
+        winnerTag.textContent = "Human Wins!";
+        winner_disp.appendChild(winnerTag);
+    }
+    else if(c_score === 5){
+        winnerTag.textContent = "Computer Wins!";
+        winner_disp.appendChild(winnerTag);
+    }
 }
 
 function playGame(humanChoice){
